b7b8a8e0e033705741cb6b072e627378393845e1
SreeHari77 <84658632+SreeHari77@users.noreply.github.com>
2021-06-30 16:13:26 +0530

    Update ari.c

--- ari.c
+++ ari.c
@@ -18,17 +18,17 @@ int index;
 index=4.71*characters/words + 0.5*words/sentences - 21.43;
 index +=1;
 if(index==1){return "Kindergarden";}
-else if(index==2){return "First/Second Grade";}
-else if(index==3){return "Third Grade";}
-else if(index==4){return "Fourth Grade";}
-else if(index==5){return "Fifth Grade";}
-else if(index==6){return "Sixth Grade";}
-else if(index==7){return "Seventh Grade";}
-else if(index==8){return "Eigth Grade";}
-else if(index==9){return "Ninth Grade";}
-else if(index==10){return "Tenth Grade";}
-else if(index=11){return "Eleventh Grade";}
-else if(index=12){return "Twelfth Grade";}
-else if(index=13){return "College Student";}
-else if(index>=14){return "Professor";}
+if(index==2){return "First/Second Grade";}
+if(index==3){return "Third Grade";}
+if(index==4){return "Fourth Grade";}
+if(index==5){return "Fifth Grade";}
+if(index==6){return "Sixth Grade";}
+if(index==7){return "Seventh Grade";}
+if(index==8){return "Eigth Grade";}
+if(index==9){return "Ninth Grade";}
+if(index==10){return "Tenth Grade";}
+if(index==11){return "Eleventh Grade";}
+if(index==12){return "Twelfth Grade";}
+if(index==13){return "College Student";}
+if(index>=14){return "Professor";}
 }
